9987ef26e47b68aa31d0194a5fccae8ef5b72111
Serjay Parks <75953992+SerjayP@users.noreply.github.com>
2022-12-07 23:23:40 -0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    img:
+      "https://i.picsum.photos/id/614/150/150.jpg?hmac=waU4GvD6BHVZ9iRivghQzcdDlXyrzs3cmkZfGvyhhjA",
+    name: "James King",
+    title: "President and CEO"
+  },
+  {
+    img:
+      "https://i.picsum.photos/id/364/150/150.jpg?hmac=YfnTs5uh8Q9y-RPqkiM1a4S8cC90xfFHyDrBqRxeqHM",
+    name: "Julie Taylor",
+    title: "VP of Marketing"
+  },
+  {
+    img:
+      "https://i.picsum.photos/id/1016/150/150.jpg?hmac=wNLVt0jsZkTjYUXCR75L0_7CurkJmlCdgXTXt5tlW1k",
+    name: "Eugene Lee ",
+    title: "CFO"
+  },
+  {
+    img:
+      "https://i.picsum.photos/id/495/150/150.jpg?hmac=4dOJy37tcMlicUiu-zezUrEW2yA5RlqOQZhpPwMPpwQ",
+    name: "John Williams",
+    title: "VP of Engineering"
+  },
+  {
+    img:
+      "https://i.picsum.photos/id/1070/150/150.jpg?hmac=Sbuzlsbt-BCe4p1hGN5kfpN2aCuOvNuQw-dJ4oY70y0",
+    name: "Lisa Moore",
+    title: "VP of Sales"
+  },
+  {
+    img:
+      "https://i.picsum.photos/id/851/150/150.jpg?hmac=QThJynmqAFEpIzhWXeFidttTkI4xEM4KpTJXQXk4IvY",
+    name: "Paul Jones",
+    title: "QA Manager"
+  }
+];
